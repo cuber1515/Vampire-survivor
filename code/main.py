ad99fbdaa423bd377c277ef98fcc1e26fa959ae2
Your Name <your.email@example.com>
2025-03-14 13:54:19 -0800

    crappy attempt at first exercise

--- code/main.py
+++ code/main.py
@@ -1,1 +1,39 @@
+import pygame
 from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player', 'down', '0.png'))
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2()
+        self.speed = 500
+
+    def update (self, dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        self.direction.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+running = True
+all_sprites = pygame.sprite.Group()
+
+player = Player(all_sprites)
+clock = pygame.time.Clock()
+
+pygame.init()
+while running:
+    dt = clock.tick() / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    all_sprites.update(dt)
+
+    screen.fill('black')
+    all_sprites.draw(screen)
+
+    pygame.display.update()
+
+pygame.quit()
