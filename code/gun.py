93251ba404ed06e39fddab269913f73e90542249
Your Name <your.email@example.com>
2025-03-18 10:42:53 -0800

    Add movement functionality to Gun class. (its goofy tho)

--- code/gun.py
+++ code/gun.py
@@ -1,7 +1,26 @@
 from settings import *
 
 class Gun(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, isMove, groups):
         super().__init__(groups)
         self.image = pygame.image.load(join('images', 'gun', 'gun.png'))
         self.rect = self.image.get_frect(center = pos)
+
+        # movement
+        self.direction = pygame.Vector2()
+        self.speed = 500
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        self.direction.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+        self.direction = self.direction.normalize() if self.direction else self.direction
+
+    def move(self, dt):
+        self.rect.x += self.direction.x * self.speed * dt
+        self.rect.y += self.direction.y * self.speed * dt
+        self.rect.center = self.rect.center
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
