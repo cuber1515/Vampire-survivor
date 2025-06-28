4753b5a68cf0bcab9a0e88c9425cf87a8acb1fda
Your Name <your.email@example.com>
2025-06-27 19:43:30 -0700

    Add Gun class and integrate with Player in the game setup

--- code/sprites.py
+++ code/sprites.py
@@ -8,7 +8,24 @@ class Sprite(pygame.sprite.Sprite):
         self.ground = True
 
 class CollisionSprite(pygame.sprite.Sprite):
+
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        # player connection
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1,0)
+
+        # sprite setup
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('images', 'gun', 'gun.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect = self.image.get_frect(center = self.player.rect.center + self.player_direction * self.distance)
+
+    def update(self, _):
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
