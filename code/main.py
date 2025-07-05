9bb64aab54f9cfb8e7be5ccb1b3139a211126027
Your Name <your.email@example.com>
2025-07-04 17:17:34 -0700

    Implement shooting mechanics with cooldown in the Game class

--- code/main.py
+++ code/main.py
@@ -21,6 +21,23 @@ class Game:
 
         self.setup()
 
+        # gun timer
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 600
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            print('shoot')
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
+
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
 
@@ -49,6 +66,8 @@ class Game:
                     self.running = False
 
             # update
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
 
             #draw
