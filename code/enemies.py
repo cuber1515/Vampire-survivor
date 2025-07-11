9ceca05d7a6c1da1c8e26ec11ccdae02c2375c86
Your Name <your.email@example.com>
2025-07-11 14:38:52 -0700

    walks around objects now

--- code/enemies.py
+++ code/enemies.py
@@ -19,6 +19,7 @@ class Enemy(pygame.sprite.Sprite):
         self.load_images()
         self.image = pygame.image.load(join('images', 'enemies', self.mob, '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
+        self.hitbox_rect = self.rect.inflate(-60, -60)
 
         # movement
         self.direction = pygame.Vector2()
@@ -41,7 +42,21 @@ class Enemy(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, dt):
-        self.rect.center += self.direction * self.speed * dt
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.hitbox_rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
+        self.rect.center = self.hitbox_rect.center
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+                else:
+                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
     
     def animate(self, dt):
         self.frame_index = self.frame_index + 10 * dt
